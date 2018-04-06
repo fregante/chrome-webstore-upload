551637d27451966a1c9be29537311ff1defc8916
Harry Mitchenson <harrymitchinson@icloud.com>
2018-04-06 15:37:03 +0100

    Update typings

--- index.d.ts
+++ index.d.ts
@@ -5,13 +5,38 @@
 
 import { ReadStream } from "fs";
 
-export enum UploadResultStates {
+/**
+ * The result state of the upload action.
+ * @export
+ */
+export enum UploadResultState {
   Failure = "FAILURE",
   InProgress = "IN_PROGRESS",
   NotFound = "NOT_FOUND",
   Success = "SUCCESS"
 }
 
+/**
+ * The result state of the publish action.
+ * @export
+ */
+export enum PublishResultStatus {
+  NotAuthorized = "NOT_AUTHORIZED",
+  InvalidDeveloper = "INVALID_DEVELOPER",
+  DeveloperNoOwnership = "DEVELOPER_NO_OWNERSHIP",
+  DeveloperSuspended = "DEVELOPER_SUSPENDED",
+  ItemNotFound = "ITEM_NOT_FOUND",
+  ItemPendingReview = "ITEM_PENDING_REVIEW",
+  ItemTakenDown = "ITEM_TAKEN_DOWN",
+  PublisherSuspended = "PUBLISHER_SUSPENDED",
+  Ok = "OK"
+}
+
+/**
+ * The APIClient options for configuration.
+ * @export
+ * @interface APIClientOptions
+ */
 export interface APIClientOptions {
   extensionId: string;
   clientId: string;
@@ -19,26 +44,49 @@ export interface APIClientOptions {
   refreshToken: string;
 }
 
+/**
+ * The response from the upload API.
+ * https://developer.chrome.com/webstore/webstore_api/items#resource
+ * @export
+ * @interface UploadResult
+ */
 export interface UploadResult {
   id: string;
   itemError: string[];
   kind: string;
   publicKey: string;
-  uploadState: UploadResultStates;
+  uploadState: UploadResultState;
 }
 
+/**
+ * The response from the publish API.
+ * https://developer.chrome.com/webstore/webstore_api/items/publish#response
+ * @export
+ * @interface PublishResult
+ */
 export interface PublishResult {
   kind: string;
   item_id: string;
-  status: string[];
+  status: PublishResultStatus[];
   statusDetail: any[];
 }
 
+/**
+ * The APIClient for interacting with the Chrome Webstore.
+ * @export
+ * @class APIClient
+ */
 export class APIClient {
-  constructor(opts: APIClientOptions);
+  constructor(options: APIClientOptions);
   uploadExisting(file: ReadStream, token: string): Promise<UploadResult>;
   publish(target: string | undefined, token: string): Promise<PublishResult>;
   fetchToken(): Promise<string>;
 }
 
-export default function(opts: APIClientOptions): APIClient;
+/**
+ * The default export function which provides an instance of the APIClient.
+ * @export
+ * @param {APIClientOptions} options 
+ * @returns {APIClient} 
+ */
+export default function(options: APIClientOptions): APIClient;
