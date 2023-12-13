7b6c6e583fde8181df6dc626239fca5bd2759417
Federico Brigante <me@fregante.com>
2023-12-13 12:21:44 +0800

    Unrelated file

--- live-test/deploy.js
+++
@@ -1,5 +0,0 @@
-/** Use ../ to deploy this extension */
-import chromeWebstoreUpload from '../index.js';
-import login from '../.env.json' assert { type: 'json' };
-
-const store = chromeWebstoreUpload(login);
